a859ea2e0b9002f9e4e4ac4ed604a723d3ba939d
juaniiie <becjuana@gmail.com>
2015-09-24 15:04:04 -0700

    Adds friends to database

--- app/www/js/friendsController.js
+++ app/www/js/friendsController.js
@@ -1,5 +1,14 @@
 angular.module('starter.friendsController', ['ionic', 'starter.services'])
 
-.controller('FriendsController', function ($scope, User) {
-  $scope.friends = User.friends;
+.controller('FriendsController', function ($scope, Friends) {
+  $scope.friends = Friends;
+
+  $scope.addFriend = function() {
+    var friendName = prompt('What is your friend\'s name?');
+    if (friendName) {
+      $scope.friends.$add({
+        'friend': friendName
+      });
+    }
+  };
 });
--- app/www/js/services.js
+++ app/www/js/services.js
@@ -1,25 +1,31 @@
 angular.module('starter.services', [])
-.factory('User', function () {
-  var friends = [
-  {
-    'name': 'Alex Manasu',
-    'number': '123'
-  },
-  {
-    'name': 'Juana Becerra',
-    'number': '456'
-  },
-  {
-    'name': 'Livvie Lin',
-    'number': '789'
-  }
-  ];
 
-  var addFriend = function (friend) {
-    // TODO
-  };
-
-  return {
-    friends: friends
-  };
+.factory('Friends', function($firebaseArray) {
+  var friendsRef = new Firebase('https://yotempest.firebaseio.com/friends');
+  return $firebaseArray(friendsRef);
 });
+
+// .factory('User', function () {
+//   var friends = [
+//   {
+//     'name': 'Alex Manasu',
+//     'number': '123'
+//   },
+//   {
+//     'name': 'Juana Becerra',
+//     'number': '456'
+//   },
+//   {
+//     'name': 'Livvie Lin',
+//     'number': '789'
+//   }
+//   ];
+
+//   var addFriend = function (friend) {
+//     // TODO
+//   };
+
+//   return {
+//     friends: friends
+//   };
+// });
