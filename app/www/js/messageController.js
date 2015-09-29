20f483f2d6496519f1a79186f1084f0a7fc033a1
Livvie Lin <livvie.lin@gmail.com>
2015-09-28 18:42:48 -0700

    Feature: Integrate friends checklist with message page

--- app/www/js/messageController.js
+++ app/www/js/messageController.js
@@ -1,9 +1,33 @@
 angular.module('starter.messageController', ['ionic', 'starter.services'])
 
-.controller('MessageController', function ($scope, Message) {
+.controller('MessageController', function ($scope, Message, Database, User) {
 
-  $scope.sendMessage = function () {
+  $scope.sendMessage = function() {
     Message.sendMessage($scope.message, $scope.token);
     $scope.message = '';
   };
+
+  // $scope.friends = Database;
+
+  // Temporary friends array
+  $scope.friends = [
+    {'name': 'Juana', 'checked': false},
+    {'name': 'Alex', 'checked': false},
+    {'name': 'Livvie', 'checked': false}
+  ];
+
+  $scope.addFriend = function () {
+    User.addFriend($scope.friends);
+  };
+
+  // Check if at least one friend selected
+  $scope.isFriendChecked = function () {
+    for (var i = 0; i < $scope.friends.length; i++) {
+      if ($scope.friends[i].checked) {
+        return true;
+      }
+    }
+    return false;
+  };
+
 });
